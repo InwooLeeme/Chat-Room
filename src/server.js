58325d85312605e936ddeb2e24c67b0ec4df84b2
ItsmeLee <arsmagica90@gmail.com>
2021-02-11 18:27:29 +0900

    Build web socket server
    Build web socket server using socket.io module.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,23 @@
 // Server side
-
+import {join} from "path";
 import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import socketIO from "socket.io";
 
 const app = express();
 
 const PORT = 5000;
 
-const home = (req, res) => res.send("Hello World")
+const home = (req, res) => res.render("home")
 
+app.set("views", join(__dirname,"views"));
+app.set('view engine', 'pug');
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(express.static(join(__dirname,"static")));
 app.get("/", home)
 
+const server = app.listen(PORT, () => console.log(`Server is Running on PORT :${PORT}`));
 
-app.listen(PORT, () => console.log(`Server is Running on PORT :${PORT}`));
+const io = socketIO.listen(server);
