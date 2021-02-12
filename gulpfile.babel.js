eb2282ca033e1a7c51504f11587230285c31bf3a
ItsmeLee <arsmagica90@gmail.com>
2021-02-12 23:39:15 +0900

    Add watch function
    Add watch files function in gulpfile.babel.js file.
    When scss file will edit, the gulp watch the files and restart the server.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,6 +7,7 @@ sass.compiler = require('node-sass');
 
 const routes = {
     style:{
+        watch:"assets/scss/*.scss",
         src:"assets/scss/styles.scss",
         dest:"src/static/styles"
     }
@@ -15,3 +16,11 @@ const routes = {
 export function styles(){
     return gulp.src(routes.style.src).pipe(sass.sync().on('error', sass.logError)).pipe(gulp.dest(routes.style.dest))
 }
+
+function watchFiles(){
+    gulp.watch(routes.style.watch, styles);
+}
+
+const dev = gulp.series([styles, watchFiles]);
+
+export default dev;
